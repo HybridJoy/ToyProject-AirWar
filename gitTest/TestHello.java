807e1ba31251ed008113f1092ea9f20b9259644c
tasun <hujunbo223@gmail.com>
2019-07-26 20:57:33 +0800

    添加 测试 java文件

---
+++ gitTest/TestHello.java
@@ -0,0 +1,12 @@
+package gitTest;
+
+public class TestHello {
+    public static void main(String[] args) {
+        int userID = getUserID();
+        System.out.println(userID + "hello world");
+    }
+
+    private static int getUserID() {
+        return 0;
+    }
+}
